9d5d255a55dded79c0bec0d1b881c996333c162e
Landoh Clansy <clensylandoh@gmail.com>
2025-06-01 05:28:46 +0000

    feat: add username input and greeting

--- number_guess.sh
+++ number_guess.sh
@@ -5,7 +5,7 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 echo "Enter your username:"
 read USERNAME
 
-# Check if user exists
+
 USER_DATA=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
 
 if [[ -z $USER_DATA ]]
